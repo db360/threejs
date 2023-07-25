a7cb55d373fe6eff389088c67093605a4d47ac2b
db360 <da.b@hotmail.es>
2023-07-25 20:13:31 +0200

    customizer

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,3 +1,12 @@
+import React, {useState, useEffect} from 'react';
+import { AnimatePresence, motion } from 'framer-motion';
+import { useSnapshot } from 'valtio';
+
+import config from '../config';
+import state from '../store';
+import {download} from '../assets';
+import {downloadCanvasToImage, reader} from '../config/helpers';
+import {EditorTags, FilterTabs, DecalTypes} from '../config/constants';
 
 const Customizer = () => {
   return (
