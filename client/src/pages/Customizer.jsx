519e5a1c8056583b3958e283f60ff2acf589c3c7
db360 <da.b@hotmail.es>
2023-07-25 20:40:46 +0200

    customizer components

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -2,15 +2,26 @@ import React, {useState, useEffect} from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 import { useSnapshot } from 'valtio';
 
-import config from '../config';
+import config from '../config/config';
 import state from '../store';
 import {download} from '../assets';
 import {downloadCanvasToImage, reader} from '../config/helpers';
 import {EditorTags, FilterTabs, DecalTypes} from '../config/constants';
+import { fadeAnimation, slideAnimation } from '../config/motion';
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components';
 
 const Customizer = () => {
+
+  const snap = useSnapshot(state);
+
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          Customizer
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
