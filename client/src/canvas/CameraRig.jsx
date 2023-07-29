0284dfa3e54c909c99f2463a9893cec3c456acda
db360 <da.b@hotmail.es>
2023-07-29 17:30:52 +0200

    Shadows, backdrop, tab Modals

--- client/src/canvas/CameraRig.jsx
+++ client/src/canvas/CameraRig.jsx
@@ -22,7 +22,7 @@ useFrame((state, delta) => {
         if(isMobile) targetPosition = [0, 0.2, 2.5]
     } else {
         if(isMobile) targetPosition = [0, 0, 2.5]
-        else targetPosition = [-0, 0, 2]
+        else targetPosition = [0, 0, 2]
     }
 
     // set model camera position
@@ -31,10 +31,10 @@ useFrame((state, delta) => {
     // set the model rotation smoothly
     easing.dampE(
         group.current.rotation,
-        [state.pointer.y / 10, -state.pointer / 5, 0],
+        [state.pointer.y / 10, -state.pointer.x / 5, 0],
         0.25,
         delta
-    )
+      )
 })
   return (
     <group ref={group}>
--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import { useState, useEffect } from "react";
 import { AnimatePresence, motion } from "framer-motion";
 import { useSnapshot } from "valtio";
@@ -19,6 +20,29 @@ import {
 const Customizer = () => {
   const snap = useSnapshot(state);
 
+  const [file, setFile] = useState('');
+  const [prompt, setPrompt] = useState('');
+  const [generatingImg, setGeneratingImg] = useState(false);
+  const [activeEditorTab, setActiveEditorTab] = useState("");
+  const [activeFilterTab, setActiveFilterTab] = useState({
+    logoShirt: true,
+    stylishShirt: false
+  })
+
+  // show tab content depnding on active tab
+  const generateTabContent = () => {
+    switch (activeEditorTab) {
+      case 'colorpicker':
+          return <ColorPicker />
+      case 'filepicker':
+          return <FilePicker />
+      case 'aipicker':
+          return <AIPicker />
+      default:
+        return null;
+    }
+  }
+
   return (
     <AnimatePresence>
       {!snap.intro && (
@@ -31,8 +55,13 @@ const Customizer = () => {
             <div className="flex items-center min-h-screen">
               <div className="editortabs-container tabs">
                 {EditorTabs.map((tab) => (
-                  <Tab key={tab.name} tab={tab} handleClick={() => {}} />
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => setActiveEditorTab(tab.name)}
+                  />
                 ))}
+                {generateTabContent()}
               </div>
             </div>
           </motion.div>
